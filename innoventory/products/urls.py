6abebaa154d465254784144e6de4171a20a9c5d0
michaela-es <michaela.destrera@gmail.com>
2025-09-30 20:42:56 +0800

    feature(products): added add product form

--- innoventory/products/urls.py
+++ innoventory/products/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.product_list, name='product-list'),  # homepage for products
+    path('', views.product_list, name='product_list'),
+    path('add/', views.add_stock, name='add_stock'),
+
 ]
--- innoventory/products/views.py
+++ innoventory/products/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import ProductForm
 from .models import Product
 
 
@@ -7,3 +9,15 @@ from .models import Product
 def product_list(request):
     products = Product.objects.all()
     return render(request, 'products/product_list.html', {'products': products})
+
+@login_required
+def add_stock(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm()
+
+    return render(request, 'products/add_stock.html', {'form': form})
