30cab10b1ae45ae5096be45ff3354ad634f77e84
michaela-es <michaela.destrera@gmail.com>
2025-10-04 18:55:11 +0800

    feature(products): edit product form added

--- innoventory/products/urls.py
+++ innoventory/products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.product_list, name='product_list'),
-    path('modal/', views.product_modal, name='product_modal'),
-    path('add/', views.add_product, name='add_product'),
+    path('modal/', views.product_modal, name='product_modal'),  # Add product
+    path('modal/<int:pk>/', views.product_modal, name='product_modal'),  # Edit product
     path('delete/<int:pk>/', views.delete_product, name='delete_product'),
 ]
